19f4d8c36f66254480cfe8acd5a0901667c50eef
John Wang <jue@alumni.cmu.edu>
2017-07-30 15:39:17 +0000

    new format

--- common/setup.sh
+++ common/setup.sh
@@ -1,13 +1,35 @@
 ### Install scripts
-if [ ! -d "~/scripts" ]; then
-  mkdir ~/scripts
-fi
-cp scripts/git-completion.bash ~/scripts/
+mkdir -p $HOME/scripts
+cp scripts/git-completion.bash $HOME/scripts/
 
 
 ### Install git config
-cp env_settings/.gitconfig ~/
+cp env_settings/.gitconfig $HOME/
 
 
 ### Install vi settings
-cp env_settings/.exrc ~/.exrc
+cp env_settings/.exrc $HOME/
+
+
+### Install bash addon to bashrc or bash_profile scripts
+BASH_FILE="$HOME/.bash_profile"
+if [ -f $HOME/.bashrc ]; then
+	BASH_FILE="$HOME/.bashrc"
+fi
+cp env_settings/.bash_addon $HOME/
+
+if grep -q bash_addon $BASH_FILE; then 
+	echo "INFO: bash init already has addon trigger"
+else
+	echo "INFO: bash init installing addon trigger"
+	echo "~/.bash_addon" >> $BASH_FILE
+fi
+if grep -q ssh-agent $BASH_FILE; then 
+	echo "INFO: bash init already has ssh-agent"
+else
+	echo "INFO: bash init installing ssh-agent"
+	echo "# starting ssh agent for adding to ssh keychain" >> $BASH_FILE
+	echo "eval \$(ssh-agent)" >> $BASH_FILE
+	echo "# add keys here e.g. ssh-add ~/.ssh/github_rsa" >> $BASH_FILE
+fi
+
--- rpi/setup.sh
+++ rpi/setup.sh
@@ -1,4 +1,7 @@
 # run common setup first
 (cd ../common ; ./setup.sh)
 
+# common setup.sh creates a .bash_addon file so append to that
+echo "# turn off capslock in favor or ctrl" >> ~/.bash_addon
+echo "setxkbmap -layout us -option ctrl:nocaps" >> ~/.bash_addon
 
