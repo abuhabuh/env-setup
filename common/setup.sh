1f777ed209e26148ba8202c7994df08914973ece
John Wang <jue@alumni.cmu.edu>
2017-09-22 11:40:29 -0400

    rename things and updates to bash

--- common/setup.sh
+++ common/setup.sh
@@ -4,11 +4,11 @@ cp scripts/git-completion.bash $HOME/scripts/
 
 
 ### Install git config
-cp env_settings/.gitconfig $HOME/
+cp env_settings/gitconfig $HOME/.gitconfig
 
 
 ### Install vi settings
-cp env_settings/.exrc $HOME/
+cp env_settings/exrc $HOME/.exrc
 
 
 ### Install bash addon to bashrc or bash_profile scripts
@@ -19,7 +19,8 @@ fi
 if [ ! -f $BASH_FILE ]; then
 	touch $BASH_FILE
 fi
-cp env_settings/.bash_addon $HOME/
+cp env_settings/bash_addon $HOME/.bash_addon
+cp env_settings/bash_ssh $HOME/.bash_ssh
 
 if grep -q bash_addon $BASH_FILE; then 
 	echo "INFO: bash init already has addon trigger"
@@ -31,12 +32,13 @@ else
 		echo "source ~/.bash_addon" >> $BASH_FILE
 	fi
 fi
-if grep -q ssh-agent $BASH_FILE; then 
-	echo "INFO: bash init already has ssh-agent"
+if grep -q bash_ssh $BASH_FILE; then 
+	echo "INFO: bash init already has ssh trigger"
 else
-	echo "INFO: bash init installing ssh-agent"
-	echo "# starting ssh agent for adding to ssh keychain" >> $BASH_FILE
-	echo "eval \$(ssh-agent)" >> $BASH_FILE
-	echo "ssh-add ~/.ssh/*" >> $BASH_FILE
+	echo "INFO: bash init installing ssh trigger"
+	if [ $BASH_FILE = "$HOME/.bashrc" ]; then
+		echo "~/.bash_ssh" >> $BASH_FILE
+	else
+		echo "source ~/.bash_ssh" >> $BASH_FILE
+	fi
 fi
-
