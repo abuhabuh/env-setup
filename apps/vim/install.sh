542e7ac014e7e75f3143d9f834a04bd6e71be622
John Wang <jue@alumni.cmu.edu>
2021-05-15 00:59:10 -0400

    vim: install colors

--- apps/vim/install.sh
+++ apps/vim/install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 #
 
-
-### Install vi settings
+# Install vi settings
 if [ ! -f "$HOME/.vimrc" ] ; then
   echo "INSTALLING $HOME/.vimrc"
   cp vimrc $HOME/.vimrc
 else
   echo "INFO: $HOME/.vimrc exists so skipping install"
 fi
+
+# Install vi colors
+if [ ! -d "$HOME/.vim/colors" ] ; then
+  echo "CREATING colors dir"
+  mkdir $HOME/.vim/colors
+fi
+
+echo "INSTALLING colors (overwriting)"
+cp -f colors/* $HOME/.vim/colors/
 
